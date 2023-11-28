ccfed772a7f54ac870a130343910eae91983c099
dalet78 <45757581+dalet78@users.noreply.github.com>
2023-11-28 22:08:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from portfolio_management.TelegramBot.bot_handler import CommandBot
+
+
+def start_bot():
+    bot = CommandBot()
+    bot.start()
+
+
+
+if __name__ == '__main__':
+    start_bot()
+
