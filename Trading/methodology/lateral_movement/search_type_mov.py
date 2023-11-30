9cfdd36647e741636760e7240920ccae55f43e06
dalet78 <45757581+dalet78@users.noreply.github.com>
2023-11-30 09:17:11 +0200

    Update search_type_mov.py

--- Trading/methodology/lateral_movement/search_type_mov.py
+++ Trading/methodology/lateral_movement/search_type_mov.py
@@ -120,12 +120,45 @@ class TrendMovementAnalyzer:
             raise ValueError("Data not found.")
 
         # RSI
-        self.data['RSI'] = self.calculate_RSI(window)
+        self.data['RSI'] = self._calculate_RSI(window)
 
         # Verify if movement is downware
         is_trend_down = self.data['RSI'] < rsi_threshold
 
         return is_trend_down
 
+    def _calculate_MACD(self, span_short=12, span_long=26, span_signal=9):
+        """
+        Calculates the Moving Average Convergence Divergence (MACD).
+        :param span_short: Span for the short-term EMA.
+        :param span_long: Span for the long-term EMA.
+        :param span_signal: Span for the signal line.
+        """
+        self.data['EMA_short'] = self.data['Close'].ewm(span=span_short, adjust=False).mean()
+        self.data['EMA_long'] = self.data['Close'].ewm(span=span_long, adjust=False).mean()
+        self.data['MACD'] = self.data['EMA_short'] - self.data['EMA_long']
+        self.data['Signal_Line'] = self.data['MACD'].ewm(span=span_signal, adjust=False).mean()
 
-    
+    def is_upward_trend_using_MACD(self):
+        """
+        Determines if there is an upward trend using MACD.
+        :return: Bool, True if there is an upward trend, False otherwise.
+        """
+        if self.data is None:
+            raise ValueError("Data not loaded.")
+
+        self._calculate_MACD()
+
+        # Check if MACD is above the signal line
+        return self.data['MACD'].iloc[-1] > self.data['Signal_Line'].iloc[-1]
+
+    def is_downward_trend_using_MACD(self):
+        """
+        Determines if there is a downward trend using MACD.
+        :return: Bool, True if there is a downward trend, False otherwise.
+        """
+        if self.data is None:
+            raise ValueError("Data not loaded.")
+
+        self._calculate_MACD()
+        return self.data['MACD'].iloc[-1] < self.data['Signal_Line'].iloc[-1]
